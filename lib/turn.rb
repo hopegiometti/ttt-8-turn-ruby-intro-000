fa1af5b6b43daf9dbea4ecc376d47030c647bd15
Hope Giometti <hopegiometti@gmail.com>
2019-07-09 19:37:19 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+#display_board
+
+#valid_move?
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0,8)
+    return true
+  end
+end
+
+#position_taken?
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+#move
